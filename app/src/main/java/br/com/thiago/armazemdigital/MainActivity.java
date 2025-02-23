9fbacf71079962a79fb6fd34f5c350675873e809
Thiago Carvalho Füllenbach <tcfullenbach@gmail.com>
2025-02-23 17:06:00 -0300

    Altera MainActivity, removendo fragment de teste e adicionando fragments base de navegação (Cadastro, Movimentação, Estoque e Configurações), além de adicionar e configurar o BottomNavigationView.

--- app/src/main/java/br/com/thiago/armazemdigital/MainActivity.java
+++ app/src/main/java/br/com/thiago/armazemdigital/MainActivity.java
@@ -1,19 +1,15 @@
 package br.com.thiago.armazemdigital;
 
 import android.os.Bundle;
-import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import br.com.thiago.armazemdigital.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
@@ -24,21 +20,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        setupNavigation();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return false;
-    }
+    private void setupNavigation() {
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(binding.contentMain.navHostFragmentContentMain.getId());
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        if(navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            NavigationUI.setupWithNavController(binding.contentMain.navBottomContentMain, navController);
+        }
     }
 }
